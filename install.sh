ff1aca8e5e82a6db71108e82e368460eeed5a89f
Jenson Bruins <jenson@jensonbruins.nl>
2022-02-14 09:54:20 +0100

    update install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt install curl -y
 
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
-sudo apt update -y
+sudo apt update -y & sudo apt upgrade -y & sudo apt install build-essentials
 
 sudo apt install ros-noetic-desktop-full -y
 
