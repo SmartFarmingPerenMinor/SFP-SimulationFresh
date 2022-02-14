7de1af3d30f327b5971b9987179236ebe6a40e22
Thomas K <thomas.kikkert@xs4all.nl>
2022-02-14 11:37:08 +0100

    fixed problem install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-sudo sh -c 'echo "deb http://packages.rog.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 sudo apt install curl -y
 
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
-sudo apt update -y & sudo apt upgrade -y & sudo apt install build-essentials
+sudo apt update -y && sudo apt upgrade -y && sudo apt install build-essentials
 
 sudo apt install ros-noetic-desktop-full -y
 
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc &
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc &&
 source ~/.bashrc
 
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-catkin python3-catkin-tools -y
 
-rosdep update & rosdep init
+rosdep update && rosdep init
 
 
