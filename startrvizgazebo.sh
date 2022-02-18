4167980ed4040b9d10c031db94913002c5d20480
Dmitri Creutzburg <dmitri.c@live.nl>
2022-02-18 13:07:26 +0100

    fix gazebo rviz launch ordering startscript

--- startrvizgazebo.sh
+++ startrvizgazebo.sh
@@ -5,12 +5,11 @@
 
 source ./catkin_ws/devel/setup.bash
 
-roslaunch ur_gazebo ur10.launch limited:=true &
-sleep 8
+roslaunch ur_gazebo ur10.launch limited:=true &&
+sleep 8 &&
 
-roslaunch ur10_e_moveit_config ur10_e_moveit_planning_execution.launch sim:=true limited:=true &
-sleep 8
+roslaunch ur10_e_moveit_config ur10_e_moveit_planning_execution.launch sim:=true limited:=true &&
+sleep 8 &&
 
-roslaunch ur10_moveit_config moveit_rviz.launch config:=true &
+roslaunch ur10_moveit_config moveit_rviz.launch config:=true &&
 sleep 8
-
