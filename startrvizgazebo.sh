7d97bfaaa858027636becfc3633a888fc9c5d8c0
Dmitri Creutzburg <dmitri.c@live.nl>
2022-02-18 14:44:01 +0100

    change startrviz to sleep 15 instead

--- startrvizgazebo.sh
+++ startrvizgazebo.sh
@@ -5,11 +5,11 @@
 
 source ./catkin_ws/devel/setup.bash
 
-roslaunch ur_gazebo ur10.launch limited:=true &&
-sleep 8 &&
+roslaunch ur_gazebo ur10.launch limited:=true &
+sleep 15
 
-roslaunch ur10_e_moveit_config ur10_e_moveit_planning_execution.launch sim:=true limited:=true &&
-sleep 8 &&
+roslaunch ur10_e_moveit_config ur10_e_moveit_planning_execution.launch sim:=true limited:=true &
+sleep 15
 
-roslaunch ur10_moveit_config moveit_rviz.launch config:=true &&
-sleep 8
+roslaunch ur10_moveit_config moveit_rviz.launch config:=true &
+sleep 15
