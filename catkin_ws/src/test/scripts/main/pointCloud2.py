46dca9e2ed2b414307b80eef88eb16e137f1f5fa
Thomas Kikkert <thomas.kikkert@xs4all.nl>
2022-03-29 10:39:46 +0200

    fixed bug in pointCloud2.py

--- catkin_ws/src/test/scripts/main/pointCloud2.py
+++ catkin_ws/src/test/scripts/main/pointCloud2.py
@@ -9,10 +9,8 @@ class PointCloud():
     points = []
 
     def __init__(self, pointCloud2_topic = "/ur10e/camera1/depth/points"):
-        #rospy.init_node('pointCloud_subscriber', anonymous=True)
         random.seed(rospy.Time.now())
         rospy.Subscriber(pointCloud2_topic, PointCloud2, self.__callback, queue_size=1)
-        rospy.spin()
 
     def __callback(self, msg):
         self.points = []
