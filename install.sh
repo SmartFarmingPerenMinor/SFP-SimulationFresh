781232aeacd5274bc3b9ee0f736cca813edb0434
Thomas Kikkert <thomas.kikkert@xs4all.nl>
2022-03-31 13:35:03 +0200

    add intelrealsense softwar to install.sh

--- install.sh
+++ install.sh
@@ -22,3 +22,10 @@ pip3 install Jinja2
 sudo rosdep init && rosdep update
 
 cd ./catkin_ws/src/test/worlds/ && ./setup.sh
+
+
+## install realsens camara software
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
+sudo apt-get install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg -y
+sudo apt-get update && sudo apt-get upgrade -y 
